675a36da64c9c424ad05cb59495e8cd9a8cde356
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2019-01-28 17:53:49 -0300

    Impressão do execell de novo

--- src/main/kotlin/br/com/astrosoft/dtw/GeraArquivo.kt
+++ src/main/kotlin/br/com/astrosoft/dtw/GeraArquivo.kt
@@ -21,27 +21,35 @@ class GeraArquivo(val arquvioExcel: String) {
       .filter { !it.codigoConta.startsWith("1.3.2.1.") }
       .groupBy { ChaveAtivoPDF(it.codigoConta, it.codigoItem) }
       .entries.mapNotNull { it.value.ordena().lastOrNull() }
-      .sortedBy { it.codigo }
+      .sortedWith(compareBy({ it.valorSaldoResidual == 0.00 }, { it.codigo }))
       .sequencial()
+/*
     dadosArquivo
       .map { it.codigo }.sorted().distinct().forEach { codigo ->
         val dados = dadosArquivo.filter { it.codigo == codigo }.sortedBy { it.sequencial }
         val dadosZerado = dados.filter { !chaveNoZerada.contains(ChaveAtivoPDF(it.codigoConta, it.codigoItem)) }
         val dadosNoZerado = dados.filter { chaveNoZerada.contains(ChaveAtivoPDF(it.codigoConta, it.codigoItem)) }
-        produzArquivosTxt("${arquvioExcel}-${i++.toString().padStart(3, '0')}-Zero-$codigo.txt", dadosZerado)
-        produzArquivosTxt("${arquvioExcel}-${i++.toString().padStart(3, '0')}-NOZero-$codigo.txt", dadosNoZerado)
+        produzArquivosExcel("${arquvioExcel}-${i++.toString().padStart(3, '0')}-Zero-$codigo.xlsx", dadosZerado)
+        produzArquivosExcel("${arquvioExcel}-${i++.toString().padStart(3, '0')}-NOZero-$codigo.xlsx", dadosNoZerado)
       }
+      */
+    val dadosZerado = dadosArquivo.filter { !chaveNoZerada.contains(ChaveAtivoPDF(it.codigoConta, it.codigoItem)) }
+    val dadosNoZerado = dadosArquivo.filter { chaveNoZerada.contains(ChaveAtivoPDF(it.codigoConta, it.codigoItem)) }
+    produzArquivosExcel("${arquvioExcel}-Zero.xlsx", dadosZerado)
+    produzArquivosExcel("${arquvioExcel}-NOZero.xlsx", dadosNoZerado)
+    produzArquivosExcel("${arquvioExcel}-Tudo.xlsx", dadosArquivo)
   }
 
-  private fun produzArquivos(arquvioExcel: String, dados: List<DadosAtivoPDF>) {
+  private fun produzArquivosExcel(arquvioExcel: String, dados: List<DadosAtivoPDF>) {
     if (dados.isEmpty()) return
     val colunas = listOf(
       Coluna("Nº do item",
              "Nº do ativo",
-             STRING) { "AT" + "${it.sequencial + 11}".padStart(5, '0') },
-      Coluna("Itens já cadastrados no SAP",
-             "Itens já cadastrados no SAP",
-             STRING) { "" },
+             STRING) { "AT" + "${it.sequencial + 15}"
+        .padStart(5, '0') },
+      /*      Coluna("Itens já cadastrados no SAP",
+                   "Itens já cadastrados no SAP",
+                   STRING) { "" },*/
       Coluna("Descrição do item",
              "Descrição do item",
              STRING) { it.descricaoItem },
@@ -56,40 +64,44 @@ class GeraArquivo(val arquvioExcel: String) {
              STRING) { it.dataEntrada.toStr() },
       Coluna("Código Área de depreciação",
              "Cód. do plano de depreciação",
-             STRING) { it.descricaoConta },
+             STRING) { "MÁQUINAS E ACES" },
       Coluna("Data de início da depreciação",
              "Geralmente é a data de aquisição",
              STRING) { it.dataEntrada.toStr() },
       Coluna("Vida útil",
              "Vida útil total",
-             STRING) { it.valorTaxa.toInt().toString() },
-      Coluna("Vida útil",
-             "em meses",
              STRING) { it.quantVidaMeses.toString() },
+      //     Coluna("Vida útil",
+      //          "em meses",
+      //       STRING) { it.quantVidaMeses.toString() },
       Coluna("Vida útil restante",
              "Vida útil restante",
-             STRING) { it.quantVidaUtil.toString() },
+             STRING) { it.quantVidaUtil.toString() }, //formula
+      Coluna("Tipo de depreciação",
+             "Geralmente é linear",
+             STRING) { "0002" },
       Coluna("CAP",
              "Custo de aquisição",
              NUMERO) { it.valorOriginal.toStr() },
       Coluna("Depreciação acumulada normal",
              "Depreciação acumulada antes do exercício",
-             NUMERO) { it.valorDepAcum?.toStr() ?: "0" },
-      Coluna("Valor Saldo Residual",
-             "Ultimo saldo residual",
-             NUMERO) { it.valorSaldoResidual?.toStr() ?: "0" }/*,
-      Coluna("Ultimo Mes",
-             "Ultimo mes/ano",
-             STRING) { it.mesAno ?: "" },
-      Coluna("taxa",
-             "taxa",
-             NUMERO) { it.valorTaxa.toStr() }*/
+             NUMERO) { it.valorDepAcum?.toStr() ?: "0" } //,
+      //Coluna("Valor Saldo Residual",
+      //     "Ultimo saldo residual",
+      //   NUMERO) { it.valorSaldoResidual?.toStr() ?: "0" },
+      /*
+    Coluna("Ultimo Mes",
+           "Ultimo mes/ano",
+           STRING) { it.mesAno ?: "" },
+    Coluna("taxa",
+           "taxa",
+           NUMERO) { it.valorTaxa.toStr() }*/
                         )
     val dadosArquivo = dados
       .groupBy { ChaveAtivoPDF(it.codigoConta, it.codigoItem) }
       .entries.mapNotNull { it.value.ordena().lastOrNull() }
-      .sortedBy { it.indice }
-      .sequencial()
+      .sortedBy { it.sequencial }
+
     val workbook = XSSFWorkbook()
     val createHelper = workbook.creationHelper
     val sheet = workbook.createSheet("Ativos")
@@ -137,10 +149,10 @@ class GeraArquivo(val arquvioExcel: String) {
     val colunas = listOf(
       Coluna("Nº do item",
              "Nº do ativo",
-             STRING) { "AT" + "${it.sequencial + 11}".padStart(5, '0') },
-      Coluna("Itens já cadastrados no SAP",
-             "Itens já cadastrados no SAP",
-             STRING) { "" },
+             STRING) { "AT" + "${it.sequencial + 15}".padStart(5, '0') },
+      /*      Coluna("Itens já cadastrados no SAP",
+                   "Itens já cadastrados no SAP",
+                   STRING) { "" },*/
       Coluna("Descrição do item",
              "Descrição do item",
              STRING) { it.descricaoItem },
@@ -155,37 +167,39 @@ class GeraArquivo(val arquvioExcel: String) {
              STRING) { it.dataEntrada.toStr() },
       Coluna("Código Área de depreciação",
              "Cód. do plano de depreciação",
-             STRING) { it.descricaoConta },
+             STRING) { "MÁQUINAS E ACES" },
       Coluna("Data de início da depreciação",
              "Geralmente é a data de aquisição",
              STRING) { it.dataEntrada.toStr() },
-      //     Coluna("Vida útil",
-      //          "Vida útil total",
-      //        STRING) { it.valorTaxa.toInt().toString() },
       Coluna("Vida útil",
-             "em meses",
+             "Vida útil total",
              STRING) { it.quantVidaMeses.toString() },
+      //     Coluna("Vida útil",
+      //          "em meses",
+      //       STRING) { it.quantVidaMeses.toString() },
       Coluna("Vida útil restante",
              "Vida útil restante",
-             STRING) { it.quantVidaUtil.toString() },
+             STRING) { it.quantVidaUtil.toString() }, //formula
+      Coluna("Tipo de depreciação",
+             "Geralmente é linear",
+             NUMERO) { "0002" },
       Coluna("CAP",
              "Custo de aquisição",
              NUMERO) { it.valorOriginal.toStr() },
       Coluna("Depreciação acumulada normal",
              "Depreciação acumulada antes do exercício",
-             NUMERO) { it.valorDepAcum?.toStr() ?: "0" },
-      Coluna("Valor Saldo Residual",
-             "Ultimo saldo residual",
-             NUMERO) { it.valorSaldoResidual?.toStr() ?: "0" }/*,
-      Coluna("Ultimo Mes",
-             "Ultimo mes/ano",
-             STRING) { it.mesAno ?: "" },
-      Coluna("taxa",
-             "taxa",
-             NUMERO) { it.valorTaxa.toStr() }*/
+             NUMERO) { it.valorDepAcum?.toStr() ?: "0" } //,
+      //Coluna("Valor Saldo Residual",
+      //     "Ultimo saldo residual",
+      //   NUMERO) { it.valorSaldoResidual?.toStr() ?: "0" },
+      /*
+    Coluna("Ultimo Mes",
+           "Ultimo mes/ano",
+           STRING) { it.mesAno ?: "" },
+    Coluna("taxa",
+           "taxa",
+           NUMERO) { it.valorTaxa.toStr() }*/
                         )
-
-
 
     colunas.forEachIndexed { index, coluna ->
     }
